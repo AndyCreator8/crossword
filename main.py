67f8312c0cd37263d62fb23310f7b3e9e33e1909
AndyCreator8 <sviderskiy.andy@gmail.com>
2025-02-19 10:58:30 +0300

    Release

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 import json
 import random
-import sys
 import time
-from pydoc import describe
+import numpy as np
 
-from colorama import init, Fore, Back, Style
+import pygame
+import screeninfo
 with open('data.json', 'r', encoding='utf8') as file:
     data = json.load(file)
-
 
 words_dict = {}
 words_coords = []
 
-def create_grid(size):
-    return [['.' for _ in range(size)] for _ in range(size)]
+def create_grid():
+    return np.full((20, 20), '.')
 
 
 def can_place_word(grid, word, row, col, is_horizontal):
@@ -71,8 +70,8 @@ def place_word(grid, word, row, col, is_horizontal):
             words_coords.append((row + i, col))
 
 
-def generate_crossword(words, size=20):
-    grid = create_grid(size)
+def generate_crossword(words, n, size=20):
+    grid = create_grid()
     while True:
         first_word = random.choice(words)
         if len(first_word) >= 12:
@@ -80,16 +79,14 @@ def generate_crossword(words, size=20):
     place_word(grid, first_word, size // 2, (size - len(first_word)) // 2, True)
     del words[words.index(first_word)]
     sc = 0
-    while sc != 6:
+    while sc != n:
         word = random.choice(words)
-        placed = False
         for i in range(400):
             row = random.randint(0, size - 1)
             col = random.randint(0, size - 1)
             orientation = random.choice([True, False])
             if can_place_word(grid, word, row, col, orientation):
                 place_word(grid, word, row, col, orientation)
-                placed = True
                 sc += 1
                 del words[words.index(word)]
                 break
@@ -99,134 +96,320 @@ def generate_crossword(words, size=20):
 def print_crossword(grid):
     for index, row in enumerate(grid):
         if index < 10:
-            print(Fore.LIGHTWHITE_EX + f"{index}  {'  '.join(row)}")
+            print(f"{index}  {'  '.join(row)}")
         else:
-            print(Fore.LIGHTWHITE_EX + f"{index} {'  '.join(row)}")
+            print(f"{index} {'  '.join(row)}")
         time.sleep(0.05)
-    print(Fore.LIGHTWHITE_EX + '   0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19')
-
+    print('   0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19')
 
 
 def mask_grid(grid, size=20):
-    masked_grid = [[Fore.LIGHTWHITE_EX + '.' for _ in range(20)] for _ in range(20)]
+    masked_grid = [['.' for _ in range(20)] for _ in range(20)]
     for i in range(20):
         for j in range(20):
             if grid[i][j] != '.':
-                masked_grid[i][j] = Fore.RED + '⊞'
+                masked_grid[i][j] = '0'
     return masked_grid
 
-# def unmask_grid(grid, size=20):
-#     for i in range(20):
-#         for j in range(20):
-#             if grid[i][j] == '⊞':
-
-
-words = list(filter(lambda x: len(x.split()) == 1, list(data.keys())))
-# print(words)
-grid = generate_crossword(words)
-# print(words_dict)
-# print(words_coords)
-masked_grid = mask_grid(grid)
-print_crossword(masked_grid)
-print_crossword(grid)
-
-
-
-
-def get_word(row, col):
-    if row > 19 or row < 0 or col > 19 or col < 0:
-        print(Fore.RED + 'Такой координаты не существует')
-        main_menu()
-    let = grid[row][col]
-    targets = {}
-    if let != '.':
-        for i in words_dict:
-            if (row, col) in words_dict[i]['coords']:
-                targets[words_dict[i]['position']] = i
-        if len(targets) == 2:
-            print(Fore.LIGHTWHITE_EX + 'Здесь 2 слова, хотите выбрать по горизонтали или вертикали? ("г" - по-горизонтали | "в" - по-вертикали)')
-            orientation = input(Fore.LIGHTWHITE_EX)
-            if orientation.lower() == 'г':
-                return data[targets[True]], True, targets[True]
-            elif orientation.lower() == 'в':
-                return data[targets[False]], True, targets[False]
-        else:
-            return data[list(targets.values())[0]], True, list(targets.values())[0]
-    else:
-        return ('Слова на такой координате нету', False)
 
 
 def open_word(word):
     for i in words_dict[word]['coords']:
-        masked_grid[i[0]][i[1]] = Fore.GREEN + word[words_dict[word]['coords'].index(i)]
+        masked_grid[i[0]][i[1]] =word[words_dict[word]['coords'].index(i)]
     del words_dict[word]
-    print_crossword(masked_grid)
-    print(Fore.LIGHTWHITE_EX + '---------------------------------------------------')
-    time.sleep(1)
-    main_menu()
+    # print_crossword(masked_grid)
+    print('---------------------------------------------------')
+    # main_menu()
 
-def guess_word(correct_word):
-    print(Fore.LIGHTWHITE_EX + 'Итак, введите слово:')
-    print(correct_word)
-    word = input(Fore.WHITE)
+
+
+words = list(filter(lambda x: len(x.split()) == 1, list(data.keys())))
+grid = generate_crossword(words, 5)
+alpha = 'йцукенгшщзхъфывапролджэячсмитьбюё'
+masked_grid = mask_grid(grid)
+# print_crossword(masked_grid)
+
+
+class Input_box:
+    def __init__(self, text, posx, posy):
+        self.text = text
+        self.text_font = Text(self.text, posx + 10, posy + 17, (width + height) // 42, (255, 255, 255))
+        self.input_box = pygame.Rect(posx, posy - 3, width / 13, height / 12)
+        self.ishidden = True
+        self.active = False
+
+
+    def render(self):
+        self.border_color = 'grey' if self.active is False else 'blue'
+        if self.ishidden is False:
+            mp = pygame.mouse.get_pos()
+            if pygame.mouse.get_pressed(num_buttons=3)[0]:
+                if self.input_box.collidepoint(mp):
+                    self.text_font.text = ''
+                    self.active = True
+                else:
+                    self.active = False
+            self.text_font.text = self.text
+            width = max(200, self.text_font.rendered_text.get_width() + 10)
+            self.input_box.w = width
+            pygame.draw.rect(screen, self.border_color, self.input_box, 2)
+            self.text_font.render()
+
+
+class Button(pygame.sprite.Sprite):
+    def __init__(self, posx, posy, text, color, text_color):
+        super().__init__(buttons)
+        self.posx = posx
+        self.posy = posy
+        self.ishidden = True
+        self.color = color
+        self.text = Text(text, self.posx + (width / 7.68) // 10, self.posy + (height / 10.8) // 4, (width + height) // 75, text_color)
+        self.box = pygame.surface.Surface((width / 7.68, height / 10.8))
+        self.buttonRect = pygame.Rect(self.posx, self.posy, width / 7.68, height / 10.8)
+
+
+    def update(self):
+        global word
+        if self.ishidden is False:
+            self.box.fill(self.color)
+            screen.blit(self.box, (self.posx, self.posy))
+            self.text.render()
+        else:
+            self.color = [50, 50, 50]
+        mp = pygame.mouse.get_pos()
+        if self.buttonRect.collidepoint(mp) and self.ishidden is False:
+            if pygame.mouse.get_pressed(num_buttons=3)[0]:
+                self.color[-1] = 200
+                self.box.fill(self.color)
+                if self.text.text == 'Угадать слово':
+                    input_box.ishidden = False
+                elif self.text.text == 'Выбрать направление':
+                    horizontal_button.ishidden = False
+                    vertical_button.ishidden = False
+                elif self.text.text == 'Горизонтальное':
+                    if brd.get_word(word[0] + 1, word[2]) is None:
+                        word = brd.get_word(word[0] - 1, word[2])
+                    else:
+                        word = brd.get_word(word[0] + 1, word[2])
+                    title.text = word[0]
+                    horizontal_button.ishidden = True
+                    vertical_button.ishidden = True
+                    choose_orientation_btn.ishidden = True
+                elif self.text.text == 'Вертикальное':
+                    if brd.get_word(word[0], word[2] + 1) is None:
+                        word = brd.get_word(word[0], word[2] - 1)
+                    else:
+                        word = brd.get_word(word[0], word[2] + 1)
+                    title.text = word[0]
+                    horizontal_button.ishidden = True
+                    vertical_button.ishidden = True
+                    choose_orientation_btn.ishidden = True
+
+            else:
+                self.color = [50, 50, 50]
+
+
+
+
+class Text:
+    def __init__(self, text, posx, posy, size, color, feed=False):
+        self.feed = feed
+        self.posx = posx
+        self.posy = posy
+        self.text = text
+        self.font = pygame.font.SysFont('Cooper', size)
+        self.color = color
+
+        self.rendered_text = self.font.render(self.text, 1, color)
+
+
+    def render(self):
+        if self.text in [i.text.text for i in buttons.sprites()]:
+            words = self.text.split()
+            for word in range(len(words)):
+                self.rendered_text = self.font.render(words[word], 1, self.color)
+                screen.blit(self.rendered_text, (self.posx, self.posy + 30 * word))
+        elif self.feed is False:
+            self.rendered_text = self.font.render(self.text, 1, self.color)
+            screen.blit(self.rendered_text, (self.posx, self.posy))
+        elif self.feed is True and len(self.text) >= 20:
+            width = 0
+            top = 0
+            words = self.text.split()
+            for word in range(len(words)):
+                self.rendered_text = self.font.render(words[word], 1, self.color)
+                screen.blit(self.rendered_text, (self.posx + width, self.posy + top))
+                width += self.font.render(words[word] + '  ', 1, (0, 0, 0)).get_width()
+                if word + 1 < len(words):
+                    if width + self.font.render(words[word + 1] + '  ', 1, (0, 0, 0)).get_width() >= 800:
+                        top += 40
+                        width = 0
+
+
+
+
+
+class Board:
+    def __init__(self, left, top, cell_size):
+        self.board = masked_grid
+        self.left = left
+        self.top = top
+        self.cell_size = cell_size
+
+
+
+    def get_cell(self, mouse_pos):
+        for y in range(len(self.board)):
+            for x in range(len(self.board[0])):
+                cell_coords = (self.cell_size * x + self.left, self.cell_size * y + self.top)
+                if (mouse_pos[0] > cell_coords[0] and mouse_pos[0] < cell_coords[0] + self.cell_size and
+                        mouse_pos[1] > cell_coords[1] and mouse_pos[1] < cell_coords[1] + self.cell_size):
+                    return x, y
+
+
+    def render(self):
+        for x in range(len(self.board)):
+            for y in range(len(self.board)):
+                if self.board[x][y] == '0':
+                    pygame.draw.rect(screen, 'red', (
+                        self.left + x * self.cell_size + 2, self.top + y * self.cell_size + 2, self.cell_size - 2, self.cell_size - 2),
+                                     )
+                elif self.board[x][y] == '1':
+                    pygame.draw.rect(screen, (200, 200, 200), (
+                        self.left + x * self.cell_size + 2, self.top + y * self.cell_size + 2, self.cell_size - 2, self.cell_size - 2))
+                elif self.board[x][y] != '.':
+                    text = Text(self.board[x][y], self.left + x * self.cell_size + self.cell_size // 8, self.top + y * self.cell_size + self.cell_size // 10, self.cell_size + 10, (255, 0, 0))
+                    text.render()
+                # elif self.board[x][y] == '1':
+                #     pygame.draw.rect(screen, 'blue', (
+                #         self.left + x * self.cell_size + 2, self.top + y * self.cell_size + 2, self.cell_size - 2, self.cell_size - 2))
+
+                pygame.draw.rect(screen, 'black', (
+                self.left + x * self.cell_size, self.top + y * self.cell_size, self.cell_size, self.cell_size), width=3)
+
+
+
+    def get_word(self, row, col):
+        let = grid[row][col]
+        targets = {}
+        if let != '.' and masked_grid[row][col] != let:
+            for i in words_dict:
+                if (row, col) in words_dict[i]['coords']:
+                    targets[words_dict[i]['position']] = i
+            if len(targets) == 2:
+                choose_orientation_btn.ishidden = False
+                return row, False, col
+            else:
+                guess_word_btn.ishidden = False
+                self.paint_word(list(targets.values())[0])
+                return data[list(targets.values())[0]], True, list(targets.values())[0]
+        else:
+            return None
+
+    def paint(self, x, y):
+        self.board[x][y] = '1'
+
+
+    def paint_word(self, word):
+        for i in words_dict[word]['coords']:
+            if self.board[i[0]][i[1]] == '0': self.board[i[0]][i[1]] = '1'
+            self.render()
+
+
+
+
+    def clear(self):
+        title.text = ''
+        status.text = ''
+        flag = False
+        input_box.active = False
+        for i in buttons.sprites():
+            i.ishidden = True
+        input_box.ishidden = True
+        for x in range(len(self.board)):
+            for y in range(len(self.board)):
+                if self.board[x][y] == '1':
+                    self.board[x][y] = '0'
+
+
+def guess_word(word, correct_word):
+    print('Итак, введите слово:')
     if word.lower() == correct_word.lower():
-        print(Fore.GREEN + 'Ого! Вы отгадали данное слово')
+        print(words_dict)
+        status.text = 'Ого! Вы отгадали данное слово'
         open_word(correct_word)
+        if words_dict == {}:
+            status.text = 'Поздравляю, вы победили!'
     else:
-        print(Fore.LIGHTWHITE_EX + 'К сожалению, это не то слово....')
-        print(Fore.LIGHTWHITE_EX + '---------------------------------------------------')
-        time.sleep(1)
-        incorrect_guess(correct_word)
-
-def incorrect_guess(correct_word):
-    print(Fore.LIGHTWHITE_EX + '("д" - продолжить отгадывать слово | "н" - выйти в главное меню)')
-    cmd = input(Fore.LIGHTWHITE_EX)
-    if cmd == 'д':
-        guess_word(correct_word)
-    elif cmd == 'н':
-        main_menu()
-    else:
-        print(Fore.LIGHTWHITE_EX + 'такой команды не существует')
-        print(Fore.LIGHTWHITE_EX + '---------------------------------------------------')
-        time.sleep(1)
-        incorrect_guess(correct_word)
-
-def try_guess(word):
-    print(Fore.LIGHTWHITE_EX + 'Хотите отгадать слово? ("д" - да, "н" - нет)')
-    a = input()
-    if a == 'д':
-        guess_word(word)
-    elif a == 'н':
-        main_menu()
-    else:
-        print(Fore.RED + 'такой команды не существует')
-        print(Fore.LIGHTWHITE_EX + '---------------------------------------------------')
-        time.sleep(1)
-        try_guess(word)
+        status.text = 'Неправильно'
 
 
-def main_menu():
-    print(words_dict)
-    if words_dict == {}:
-        print(Fore.GREEN + 'Поздравляем, вы выиграли!!!')
-        return
-    print(Fore.LIGHTWHITE_EX + 'Введите одну координату слова')
-    i = input(Fore.LIGHTWHITE_EX)
-    coords = i.split()
-    try:
-        result = get_word(int(coords[0]), int(coords[1]))
-    except Exception:
-        print(Fore.RED + 'Неверный формат ввода')
-        main_menu()
-    describtion = result[0].replace('. ', '\n')
-    print(describtion)
-    if result[1] == True:
-        try_guess(result[2])
-    else:
-        print(Fore.LIGHTWHITE_EX + '---------------------------------------------------')
-        time.sleep(1)
-        main_menu()
 
-main_menu()
-# print('\n'.join([data[i] for i in words_coords.keys()]))
+for i in screeninfo.get_monitors():
+    height, width = i.height, i.width
+    break
 
+size = width, height = 1920, 1080
+brd = Board(10, 50, (width + height) // 60)
+pygame.init()
+
+screen = pygame.display.set_mode(size)
+space = pygame.image.load('фон космоса.jpg')
+space = pygame.transform.scale(space, (width, height))
+title = Text('', width // 1.84, height // 54 + 50, (width + height) // 50, (255, 255, 255), True)
+running = True
+buttons = pygame.sprite.Group()
+status = Text('', width // 1.84, height // 54 + 320, (width + height) // 50, (255, 255, 255), False)
+guess_word_btn = Button(width // 1.84, height // 54 + 400, 'Угадать слово', [50, 50, 50], (255, 255, 255), )
+choose_orientation_btn = Button(width // 1.84, height // 54 + 400, 'Выбрать направление', [50, 50, 50], (255, 255, 255))
+horizontal_button = Button(width // 1.84, 300, 'Горизонтальное', [50, 50, 50], (255, 255, 255))
+vertical_button = Button(width // 1.84 + 275, 300, 'Вертикальное', [50, 50, 50], (255, 255, 255))
+description = Text('Описание: ', width // 1.84, height // 54, (width + height) // 50, (255, 255, 255))
+input_box = Input_box('', width // 1.84, height // 54 + 200)
+clock = pygame.time.Clock()
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            key = pygame.key.get_pressed()
+            if input_box.active:
+                if key[pygame.K_RETURN]:
+                    guess_word(input_box.text, word[-1])
+                    input_box.text = ''
+                elif key[pygame.K_BACKSPACE]:
+                    input_box.text = input_box.text[:-1]
+                else:
+                    input_box.text += event.unicode
+            if key[pygame.K_LSHIFT]:
+                brd.board = grid
+        elif event.type == pygame.KEYUP:
+            brd.board = masked_grid
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                pos = brd.get_cell(event.pos)
+                if pos:
+                    if brd.get_word(*pos):
+                        word = brd.get_word(*pos)
+                        if word[1] is True:
+                            brd.clear()
+                            brd.paint(*pos)
+                            title.text = brd.get_word(*pos)[0]
+                        elif word[1] is False:
+                            brd.clear()
+                            brd.paint(*pos)
+                            brd.get_word(*pos)
+
+
+    screen.fill((255,255,255))
+    screen.blit(space, (0, 0))
+    brd.render()
+    title.render()
+    buttons.update()
+    input_box.render()
+    description.render()
+    status.render()
+
+    clock.tick(30)
+    pygame.display.flip()
+pygame.quit()
